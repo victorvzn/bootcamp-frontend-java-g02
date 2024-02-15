e6b77c59aeee1753d109af7b41e584fc095d43f9
Victor Villazón <victorzn23@gmail.com>
2024-02-14 20:53:50 -0500

    semana2, dia02, javascript basico

--- semana02/dia02-javascript-basico/app.js
+++ semana02/dia02-javascript-basico/app.js
@@ -1,38 +1,45 @@
-console.log('Hello world from index.js!')
+console.log('¡Hola Javascript desde app.js!');
 
-// Esto es un comentario en un línea
+// Esto es un comentario en una línea
 
-/* Esto
-es 
-un
-comentario
-de
-múltiples
-líneas
+/*
+  Esto
+  es
+  un
+  comentario
+  de
+  múltiples
+  líneas
 */
 
 // TIPOS DE DATOS
 
-// PRIMITIVOS: Number, String, Boolean (true, false), undefined
+// PRIMITIVOS: Number, String, Boolean (true, false), undefined, null
 
 // NUMBER
-const numero1 = 20
-const numero2 = 20.50
-const numero3 = -36
+const numero1 = 20;
+const numero2 = 20.50;
+const numero3 = -36;
 
-console.log(numero1)
-console.log(numero2)
-console.log(numero3)
+console.log(numero1);
+console.log(numero2);
+console.log(numero3);
 
-console.log(typeof numero1)
-console.log(typeof numero2)
-console.log(typeof numero3)
+console.log(typeof numero1);
+console.log(typeof numero2);
+console.log(typeof numero3);
 
 // STRING o cadena de texto
+
 const alumna = 'Claudia'
-const alumno = "Marco"
+const alumno = 'Marco'
 const numero4 = "200"
 const numero5 = 200
+
+console.log(alumna)
+console.log(alumno)
+console.log(numero4)
+console.log(numero5)
 
 console.log(typeof alumna)
 console.log(typeof alumno)
@@ -53,7 +60,7 @@ let nombre
 console.log(nombre)
 console.log(typeof nombre)
 
-// NULL -> Es un object
+// NULL
 
 let apellido = null
 
@@ -61,24 +68,21 @@ console.log(apellido)
 console.log(typeof apellido)
 
 // Object
-// Funtion
+// Function
 // BigInt
 // Symbol
 
+// VARIABLES Y CONSTANTES
 
-// VARIABLES Y CONSTANSTANTES
+// ESCMACRIPT 5 === ES5
+// ESCMACRIPT 6 === ES6
 
-// ECMASCRIPT 5 === ES5
-// ECMASCRIPT 6 === ES6
-
-// var -> ES5 (Forma de declarar una variable)
+// var -> ES5 (Forma de declarar una variable pero ya no es recomendada) ❌
 var nombreDeMiVariable = 'un valor'
 
 // CONST -> ES6 (El uso de const es lo recomendado)
 
-// * No podemos reasignar su valor
-
-const edad = 55
+const edad = 55  // Es obligatorio inicializarlo
 const _edad = 25
 const edadDeMiHijo = 15
 
@@ -86,18 +90,15 @@ const edadDeMiHijo = 15
 
 console.log(edad)
 
-// LET -> ES6 (El uso de let es lo recomendado)
+// LET -> ES6 (EL uso de let es lo recomendado)
 
-// * Sí podemos reasignar su valor
+let edad2 = 35 // No es obligatorio inicializarlo
 
-let edad2 = 35
-edad2 = 36
+edad2 = "36"
+edad2 = true
 edad2 = 22
 
 console.log(edad2)
-console.log(typeof edad2)
-
-edad2 = "hola"
 console.log(typeof edad2)
 
 // OPERADORES MATEMÁTICOS
@@ -109,6 +110,9 @@ console.log(1 / 2)
 console.log(1 % 2) // Residuo
 console.log(2 ** 3) // dos elevado al cubo
 
+// console.log(Math.pow(2, 3)) // Hace lo mismo que 2 ** 3
+
+
 // OPERADOR DE ASIGNACIÓN
 
 const genero = 'femenino'
@@ -118,12 +122,12 @@ console.log(genero + '*')
 // OPERADORES DE COMPARACIÓN
 
 // Operador de igualdad y desigualdad no estricta
-// DEvuelven un valor booleano(true o false)
+// Devuelven un valor booleano (true o false)
 // Este operador no toma en cuenta el tipo de dato al comparar sino solo sus valores
 
 console.log(1 == 1) // true
 console.log(1 == '1') // true
-console.log(1 != 1) // false
+console.log(1 != '1') // false ! =
 
 // Operador de igualdad y desigualdad estricta (ES RECOMENDABLE USARLO CASI SIEMPRE)
 // Devuelven un valor booleano(true o false)
@@ -137,8 +141,8 @@ console.log(1 !== 1) // false ! ==
 
 console.log(8 > 5)
 console.log(5 < 1)
-console.log(8 >= 5)
-console.log(8 <= 5)
+console.log(8 >= 5) // > =
+console.log(8 <= 5) // < =
 
 // Operadores lógicos (AND, OR, NEGACIÓN)
 
@@ -152,7 +156,7 @@ const saludo = 'Hola, '
 const nombreCompleto = 'Victor' + ' ' + 'Villazón'
 const miEdad = 37
 
-const saludoAmigo = saludo + nombreCompleto + '. Tengo ' + miEdad + ' años'
+const saludoAmigo = saludo + nombreCompleto + ". Tengo " + miEdad + ' años'
 
 console.log(nombreCompleto)
 console.log(saludoAmigo)
@@ -207,10 +211,10 @@ console.log(area) // 25
 // 8. (TODO) Almacenar en una constante un monto de dinero, luego mostrar cuanto le toca a cada socio según la siguiente tabla:
 // SOCIO A = 30%, SOCIO B = 20%, SOCIO C = 60%
 
-// CONDICIONALES (IF )
+// CONDICIONALES (IF)
 
 if (true) {
-  // Verdadero
+  // El bloque que se ejecutar si la condición es verdadera (true)
 }
 
 const numero = 33
@@ -218,175 +222,59 @@ const numero = 33
 const esPar = numero % 2 === 0
 
 if (esPar) {
-  console.log('Este numero es par')
+  console.log('Este número es par')
 }
 
 // CONDICIONALES (IF, ELSE)
 
 if (true) {
-  // Verdadero
+  // Se ejecuta este bloque si la condición es verdadera
 } else {
-  // Falso
+  // Se ejecuta este bloque si la condición es falsa
 }
 
 if (esPar) {
-   console.log('Este numero es par')
+  console.log('Este número es par')
 } else {
-  console.log('Este numero es impar')
+  console.log('Este número es impar')
 }
 
 // CONDICIONALES (IF, ELSE IF, ELSE)
 
-if (true) {
-  // Verdadero
-} else { }
+// if (true) {
+//   // Se ejecuta este bloque si la condición es verdadera
+// } else if (true) {
+//   // Se ejecuta este bloque si la condición es verdadera
+// } else if (true) {
+//   // Se ejecuta este bloque si la condición es verdadera
+// } else {
+//   // Se ejecuta este bloque si ninguna condición se cumple
+// }
 
 const heroe = 'Spiderman'
-// const heroe = 'Victor'
 
 if (heroe === 'Batman') {
-  console.log('Hola soy Bruce')
+  console.log('HOla soy Bruce')
 } else if (heroe === 'Spiderman') {
   console.log('Hola soy Peter')
 } else if (heroe === 'Ironman') {
   console.log('Hola soy Tony')
 } else {
-  console.log('No soy un heroe aún')
+  console.log('No soy un heroe aún :(')
 }
 
 // CONDICIONALES (SWITCH)
 
 switch (heroe) {
-  case 'Barman':
-    console.log('Hola soy Bruce');
+  case 'Batman':
+    console.log('Hola soy Bruce')
     break
   case 'Spiderman':
-      console.log('Hola soy Peter');
-      break
+    console.log('Hola soy Peter')
+    break
   case 'Ironman':
     console.log('Hola soy Tony')
     break
   default:
-    console.log('No soy un heroe aún')
+    console.log('No soy un heroe aún :(')
 }
-
-// ESTRUCTURAS REPETITIVAS
-
-// FOR (Sirve para repetir una o varias instrucciones)
-
-for (let i = 0; i < 10; i++) {
-  console.log(i)
-}
-
-// WHILE
-
-let j = 0
-
-while (j < 10) {
-  console.log(j)
-  j = j + 1
-  // j++
-}
-
-// DO WHILE
-
-let k = 0
-
-do {
-  console.log(k)
-  k = k + 1
-  // k++
-} while (k < 10)
-
-// EJERCICIOS
-  
-// 1. Determinar si la edad de una persona es mayor de edad o menor de edad mostrando 'Mayor de edad' o 'Menor de edad'
-
-const edadPersona = 37
-
-if (edadPersona >= 18) {
-  console.log('Mayor de edad')
-} else {
-  console.log('Menor de edad')
-}
-
-// 2. Retornar un saludo en tres diferentes lenguajes:
-//    - si es español mostrará 'Hola'
-//    - si es inglés mostrará 'Hello'
-//    - si es aimara mostrará 'kamisaraki'
-// 3. Si un numero es divisible entre 3 y 5 devolver 'fizzbuzz'
-//    Si un numero es divisible entre 3 devolver 'fizz'
-//    Si un numero es divisible entre 5 devolver 'buzz'
-//    De lo contrario devolver el mismo número
-// 4. REtornar true si un numero es primo sino devolver false
-//    Pista: un numero primo es divisible por sí mismo y por 1
-// 5. Devolver un valor que se incremente de dos en dos y devuelva los menores a 10
-
-//  FUNCIONES
-
-// Definiendo una función
-
-function nombreDelaFuncion() {
-  // Cuerpo de la función y aquí va la lógica a ejecutar
-}
-nombreDelaFuncion()
-
-// Funciones sin parámetros
-
-function imprimirMiNombre() {
-  console.log('Hola soy Victor')
-  console.log('Y soy Frontend')
-}
-
-imprimirMiNombre()
-
-// Funciones con parámetros
-
-function imprimirUnNombre(nombre) {
-  console.log('Hola soy ' + nombre)
-}
-
-imprimirUnNombre('Claudia')
-
-function imprimirNombreYApellido(nombre, apellido, edad) {
-  // console.log('Hola soy ' + nombre + ' ' + apellido + ' y tengo ' + edad + ' años.')
-  console.log(`Hola soy ${nombre} ${apellido} y tengo ${edad} años.`)
-}
-
-imprimirNombreYApellido('Victor', 'Villazón', 37)
-
-// EJERCICIOS
-
-// 1. Usando funciones determinar si la edad de una persona es mayor de edad o menor de edad mostrando 'Mayor de edad' o 'Menor de edad'
-
-// Funciones sin retorno
-
-function esMayorOMenorDeEdad(edad) {
-  if (edad >= 18) {
-    console.log('Mayor de edad')
-  } else {
-    console.log('Menor de edad')
-  }
-}
-
-esMayorOMenorDeEdad(37)
-esMayorOMenorDeEdad(7)
-esMayorOMenorDeEdad(3)
-
-// Funciones con retorno de valor
-
-function esMayorOMenorDeEdadConRetorno(edad ) {
-  if (edad >= 18) {
-    return 'Mayor de edad'
-  } else {
-    return 'Menor de edad'
-  }
-}
-
-console.log(esMayorOMenorDeEdadConRetorno(25))
-console.log(esMayorOMenorDeEdadConRetorno(15))
-
-// PROMPT para leer valores del usuario
-// const tuEdad = prompt('Ingresa tu edad')
-
-// console.log(tuEdad)

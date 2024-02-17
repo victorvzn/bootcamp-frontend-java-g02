12f6cd07e680a0c0256ffc0ea7ef1ef963217683
Victor Villazón <victorzn23@gmail.com>
2024-02-16 19:32:23 -0500

    semana2, dia03, javascript intermedio - arrays

---
+++ semana02/dia03-javascript-intermedio/js/index.js
@@ -0,0 +1,25 @@
+console.log('Hola JS')
+
+// Tipos de datos primitivos
+
+const numero = 399 // Number
+const cadena = 'Victor' // String
+const esMayor = true // Boolean
+
+// Arrays
+
+// Un arreglo tiene elementos a cualquier tipo de dato: cadena, números, boolean, null, arrays, objetos, etc...
+
+const arregloVacio = []
+const listaDeValores = [1, 2, 3, 'Victor', 'Villazón', true]
+
+console.log(arregloVacio)
+console.log(listaDeValores)
+
+// Lectura de los elementos de un arreglo
+
+console.log(listaDeValores[0]) // 1
+console.log(listaDeValores[4]) // Villazón
+console.log(listaDeValores[5]) // true
+console.log(listaDeValores[100]) // undefined
+

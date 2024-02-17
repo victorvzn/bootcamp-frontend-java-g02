bc13e2dcff2683151af85d169f1d1eeeeb9f1c22
Victor Villazón <victorzn23@gmail.com>
2024-02-16 22:32:33 -0500

    semana2, dia03, javascript intermedio - todo app

--- semana02/dia03-javascript-dom-todo-app/main.js
+++ semana02/dia03-javascript-dom-todo-app/main.js
@@ -1,1 +1,17 @@
 console.log('Hola JS')
+
+const taskAdd = document.querySelector('.task__add')
+const taskInput = document.querySelector('.task__input')
+// const taskInput = document.querySelector('#taskInput')
+// const taskInput = document.getElementById('taskInput')
+const taskList = document.querySelector('.task__list')
+
+// console.log(taskAdd)
+// console.log(taskInput)
+// console.log(taskList)
+
+taskAdd.addEventListener('click', function (event) {
+  // Se ejecutará cuando hagamos click en el botón "Añadir tarea"
+  console.log('Click!')
+})
+

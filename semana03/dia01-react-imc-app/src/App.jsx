a942100b0e7c265e6181c1c980ae2f1129515e69
Victor Villazón <victorzn23@gmail.com>
2024-02-19 22:50:36 -0500

    semana3, dia01, react imc app

--- semana03/dia01-react-imc-app/src/App.jsx
+++ semana03/dia01-react-imc-app/src/App.jsx
@@ -1,33 +1,74 @@
+import { useState } from "react"
+
 const App = () => {
+  const [weight, setWeight] = useState(0)
+  const [height, setHeight] = useState(0)
+
+  const handleWeight = (event) => {
+    const value = event.target.value
+
+    setWeight(value)
+  }
+
+  const handleHeight = (event) => {
+    const value = event.target.value
+
+    setHeight(value)
+  }
+
+  const bodyMassIndex = weight / ((height / 100) ** 2)
+
+  const bodyMassIndexResult = () => {
+    let result = ''
+
+    if (bodyMassIndex < 18.5) {
+      result = 'Baja'
+    } else if (bodyMassIndex >= 18.5 && bodyMassIndex <= 24.9) {
+      result = 'Normal'
+    } else if (bodyMassIndex >= 25 && bodyMassIndex <= 29.9) {
+      result = 'Sobrepeso'
+    } else if (bodyMassIndex >= 30) {
+      result = 'Obeso'
+    } else {
+      result = 'Datos icorrectos'
+    }
+
+    return result
+  }
+
   return (
-    <section>
-      <h1>IMC Calculator</h1>
+    <section className="w-96 border mx-auto bg-sky-500 rounded-md my-5">
+      <h1 className="text-3xl font-bold text-center text-white p-5">IMC Calculator</h1>
 
-      <div>
+      <div className="bg-sky-200 p-5">
         <label>
-          Weight: 0 kg
+          <span className="font-bold block">Weight: {weight} kg</span>
           <input
+            className="w-full"
             type="range"
             min="50"
             max="200"
+            onChange={handleWeight}
           />
         </label>
 
         <label>
-          Height: 0 cm
+        <span className="font-bold block">Height: {height} cm</span>
           <input
+            className="w-full"
             type="range"
             min="50"
             max="200"
+            onChange={handleHeight}
           />
         </label>
 
-        <p>
-          Tu IMC es 0.00
+        <p className="text-2xl font-bold bg-orange-400 text-center p-5 mt-5 rounded-md">
+          Tu IMC es {bodyMassIndex.toFixed(2)}
         </p>
 
-        <p>
-          Estado de IMC: Normal / Obeso / ...
+        <p className="text-2xl font-bold bg-orange-500 text-center p-5 mt-5 rounded-md">
+          Estado de IMC: {bodyMassIndexResult()}
         </p>
 
       </div>

0e854624d76495b408e214703028183c04f860a4
Victor Villazón <victorzn23@gmail.com>
2024-02-21 19:51:37 -0500

    semana3, dia02, react todo app

--- semana03/dia02-react-todo-app/src/App.jsx
+++ semana03/dia02-react-todo-app/src/App.jsx
@@ -22,10 +22,12 @@ const App = () => {
   const [todos, setTodos] = useState(DEFAULT_TODOS)
 
   return (
-    <main>
-      
+    <main
+      className="bg-yellow-100 w-full max-w-sm mx-auto mt-10 border border-yellow-600 rounded-lg p-4 shadow-2xl"
+    >
+      <h1 className="text-2xl font-bold text-center">TODO APP</h1>
 
-      <pre>{JSON.stringify(todos, null, 2)}</pre>
+      {/* <pre className="border bg-slate-50 rounded-md">{JSON.stringify(todos, null, 2)}</pre> */}
     </main>
   )
 }

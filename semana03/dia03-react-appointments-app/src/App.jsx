001b0b450de1068a3bc13dcdeb432d238ac850c8
Victor Villazón <victorzn23@gmail.com>
2024-02-23 19:46:44 -0500

    semana3, dia03, react appointments app

--- semana03/dia03-react-appointments-app/src/App.jsx
+++ semana03/dia03-react-appointments-app/src/App.jsx
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 export default function App() {
   return (
     <>
-      <Header />
+      <Header title="Citas médicas para mascotas" />
     </>
   )
 }
--- semana03/dia03-react-appointments-app/src/components/Header.jsx
+++ semana03/dia03-react-appointments-app/src/components/Header.jsx
@@ -1,9 +1,20 @@
-const Header = () => {
+const Header = ({ title }) => {
   return (
     <h1 className="text-4xl text-center mt-4">
-      Citas médicas para mascotas
+      {title}
     </h1>
   )
 }
 
 export default Header
+
+
+// const Header = (props) => {
+//   return (
+//     <h1 className="text-4xl text-center mt-4">
+//       {props.title}
+//     </h1>
+//   )
+// }
+
+// export default Header

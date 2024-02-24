2ce3ed10ca25c624f5ac31add827921c701b164f
Victor Villazón <victorzn23@gmail.com>
2024-02-23 20:16:10 -0500

    semana3, dia03, react appointments app

--- semana03/dia03-react-appointments-app/src/pages/Appointments.jsx
+++ semana03/dia03-react-appointments-app/src/pages/Appointments.jsx
@@ -1,21 +1,21 @@
 const Appointments = () => {
   return (
     <>
-      <section>
-        <h2>Nuevo paciente</h2>
+      <section className="w-96 p-4">
+        <h2 className="text-2xl text-center mb-4">Nuevo paciente</h2>
 
-        <form>
+        <form className="flex flex-col gap-4">
           <input
             type="text"
             name="petName"
             placeholder="Nombre de mi mascota"
-            className=""
+            className="border p-3 shadow-md rounded-md"
           />
           <input
             type="number"
             name="petAge"
             placeholder="Edad de la mascota"
-            className=""
+            className="border p-3 shadow-md rounded-md"
             min="0"
             max="50"
           />
@@ -23,29 +23,29 @@ const Appointments = () => {
             type="text"
             name="ownerName"
             placeholder="Dueño de la mascota"
-            className=""
+            className="border p-3 shadow-md rounded-md"
           />
           <input
             type="date"
             name="appointmentDate"
             placeholder="Fecha de la cita"
-            className=""
+            className="border p-3 shadow-md rounded-md"
           />
           <input
             type="time"
             name="appointmentTime"
             placeholder="Fecha de la cita"
-            className=""
+            className="border p-3 shadow-md rounded-md"
           />
           <textarea
             name="symptoms"
             placeholder="Síntomas"
-            className=""
+            className="border p-3 shadow-md rounded-md"
             rows="3"
           />
           <input
             type="submit"
-            className=""
+            className="border p-2 bg-green-800 text-white rounded-md cursor-pointer"
             value="Guardar"
           />
         </form>

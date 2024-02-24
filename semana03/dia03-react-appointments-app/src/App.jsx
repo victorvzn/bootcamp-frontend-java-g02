fa3252ae6f8b76514cd4aeff26e602183e0bb64b
Victor Villazón <victorzn23@gmail.com>
2024-02-23 19:52:16 -0500

    semana3, dia03, react appointments app

--- semana03/dia03-react-appointments-app/src/App.jsx
+++ semana03/dia03-react-appointments-app/src/App.jsx
@@ -4,6 +4,11 @@ export default function App() {
   return (
     <>
       <Header title="Citas médicas para mascotas" />
+
+      <main className="container border border-red-500 mx-auto flex gap-12 py-5">
+        <div>FORM</div>
+        <div>LIST</div>
+      </main>
     </>
   )
 }

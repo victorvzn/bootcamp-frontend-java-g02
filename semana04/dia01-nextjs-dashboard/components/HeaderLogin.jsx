5649ad7a37e1084691b1b3e62b19f1b1ed635ca1
Victor Villazón <victorzn23@gmail.com>
2024-02-26 22:52:14 -0500

    semana4, dia01, nexts dashboard

--- semana04/dia01-nextjs-dashboard/components/HeaderLogin.jsx
+++ semana04/dia01-nextjs-dashboard/components/HeaderLogin.jsx
@@ -2,12 +2,12 @@ import Link from 'next/link'
 
 export default function HeaderLogin() {
   return (
-    <header className="bg-sky-700 text-white py-4 px-6">
+    <header className="bg-sky-700 text-white py-4 px-6 fixed w-full">
       <div className="container mx-auto flex justify-between">
         <h1 className="font-bold">
           <Link
-            href='/home'
-            className="hover:text-amber-800"
+            href='/'
+            className="hover:text-amber-400"
           >
             Budget App
           </Link>

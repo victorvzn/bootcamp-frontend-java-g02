e20bb72dc1b68a4994aa673d42fa53a8de108618
Victor Villazón <victorzn23@gmail.com>
2024-02-26 20:54:18 -0500

    semana3, dia03, react appointments app

---
+++ semana04/dia01-nextjs-intro/app/home/products/[id]/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ProductDetail({ params }) {
+  return (
+    <div>ProductDetail {params.id}</div>
+  )
+}
--- semana04/dia01-nextjs-intro/app/home/products/page.jsx
+++ semana04/dia01-nextjs-intro/app/home/products/page.jsx
@@ -1,7 +1,22 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function ProductsPage() {
   return (
-    <div>Products Page</div>
+    <>
+      <div>Products Page</div>
+
+      <ul>
+        <li>
+          <Link href={`/home/products/1`}>Product 1</Link>
+        </li>
+        <li>
+        <Link href={`/home/products/2`}>Product 2</Link>
+        </li>
+        <li>
+        <Link href={`/home/products/3`}>Product 3</Link>
+        </li>
+      </ul>
+    </>
   )
 }

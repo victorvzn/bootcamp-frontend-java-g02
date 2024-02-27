eb1027bc2fec43b940e52537ef795b798f27d103
Victor Villazón <victorzn23@gmail.com>
2024-02-26 20:39:13 -0500

    semana4, dia01, nextjs intro

--- semana04/dia01-nextjs-intro/app/login/page.jsx
+++ semana04/dia01-nextjs-intro/app/login/page.jsx
@@ -1,7 +1,5 @@
-function LoginPage() {
+export default function LoginPage() {
   return (
     <div>Login Page</div>
   )
 }
-
-export default LoginPage
--- semana04/dia01-nextjs-intro/app/page.js
+++ semana04/dia01-nextjs-intro/app/page.js
@@ -1,5 +1,15 @@
+import Link from 'next/link'
+
 export default function Page() {
   return (
-    <h1>Hola Next.js</h1>
+    <>
+      <nav className="flex gap-2">
+        <Link href="/login" className="text-blue-600">Login</Link>
+        <Link href="/home" className="text-blue-600">Home</Link>
+        <Link href="/home/products" className="text-blue-600">Products</Link>
+      </nav>
+
+      <h1>Hola Next.js</h1>
+    </>
   );
 }

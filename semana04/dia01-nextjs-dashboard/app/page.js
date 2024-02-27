918c97c35e4a368f65deb11e95aad1972891e482
Victor Villazón <victorzn23@gmail.com>
2024-02-26 22:02:16 -0500

    semana4, dia01, nexts dashboard

--- semana04/dia01-nextjs-dashboard/app/page.js
+++ semana04/dia01-nextjs-dashboard/app/page.js
@@ -1,23 +1,27 @@
+import Link from "next/link";
+
 export default function LoginPage() {
   return (
-    <main className="container border border-red-500 w-[400px] h-screen mx-auto space-y-6 flex flex-col justify-center">
-      <div className="mx-auto flex w-full flex-col justify-center space-y-10">
+    <main className="containe w-[400px] h-screen mx-auto space-y-6 flex flex-col justify-center">
+      <div className="mx-auto flex w-full flex-col justify-center">
 
         <div className="flex flex-col space-y-2 text-center">
           <h1 className="text-4xl font-semibold">Login</h1>
           <p className="text-sm text-slate-600">Enter your email below to login your account.</p>
         </div>
 
-        <div className="flex flex-col gap-6">
-          <label>
-            Email
-            <input type="email" placeholder="jhondoe@mail.com" />
-          </label>
-          <label>
-            Password
-            <input type="password" placeholder="Ej. supersecret" />
-          </label>
-          <button>Login</button>
+        <form className="flex flex-col gap-2 p-8">
+          <label className="font-bold">Email</label>
+          <input className="border p-3 shadow-sm rounded-md" type="email" placeholder="jhondoe@mail.com" />
+          
+          <label className="font-bold">Password</label>
+          <input className="border p-3 shadow-sm rounded-sm" type="password" placeholder="Ej. supersecret" />
+          
+          <button className="mt-4 border p-3 bg-sky-700 text-white rounded-md cursor-pointer">Login</button>
+        </form>
+
+        <div className="flex justify-center gap-2 font-bold">
+          <span>Don't have an account?</span> <Link href="/register" className="text-sky-500">Register</Link>
         </div>
 
       </div>

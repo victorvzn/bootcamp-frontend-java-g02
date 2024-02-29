846d197552a9d7ad65f4869992bf2228fc61f27c
Victor Villazón <victorzn23@gmail.com>
2024-02-28 22:53:22 -0500

    semana4, dia02, budget app

--- semana04/dia02-nextjs-budgets-app/app/home/page.jsx
+++ semana04/dia02-nextjs-budgets-app/app/home/page.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import MonitorBudget from '@/components/MonitorBudget'
 
 export default function HomePage() {
   return (
-    <div>Home Page</div>
+    <>
+      <MonitorBudget />
+
+      <h1 className='text-3xl my-4'>Expense list</h1>
+
+      <ul className='flex flex-col gap-4'>
+        <li>
+          expense.name - S/ expense.amount <button className='bg-red-600 px-2 text-white'>Delete</button>
+        </li>
+        <li>expense.name - S/ expense.amount <button className='bg-red-600 px-2 text-white'>Delete</button></li>
+        <li>expense.name - S/ expense.amount <button className='bg-red-600 px-2 text-white'>Delete</button></li>
+      </ul>
+    </>
   )
 }

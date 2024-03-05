9acb23d2f8a9302a5846087dd7149a4e22d87ef3
Victor Villazón <victorzn23@gmail.com>
2024-03-04 22:23:53 -0500

    semana5, dia01, invoice app part1

--- semana04/dia03-nextjs-budgets-app/app/invoices/page.jsx
+++ semana04/dia03-nextjs-budgets-app/app/invoices/page.jsx
@@ -1,11 +1,12 @@
 'use client'
 
 import { TbSquareRoundedPlus } from "react-icons/tb";
+import { TbChevronRight } from "react-icons/tb";
 
 export default function InvoicesPage() {
   return (
     <>
-    <header className="flex justify-between items-center container mx-auto bg-slate-900 my-5">
+      <header className="flex justify-between items-center container mx-auto bg-slate-900 my-5">
         <div className="text-white">
           <h1 className="text-4xl font-extrabold">Invoices</h1>
           <span>There are 7 total invoices</span>
@@ -18,6 +19,27 @@ export default function InvoicesPage() {
           </button>
         </div>
       </header>
+
+      <section>
+        <article className="text-white2">
+          <div>
+            <span>#</span>
+            <span>RT3080</span>
+          </div>
+          <div>Due 19 Feb 2024</div>
+          <div>Jensen Huang</div>
+          <div>$ 1,800.90</div>
+          <div>
+            <div>
+              <span>•</span>
+              <span>Pending</span>
+            </div>
+          </div>
+          <div>
+            <TbChevronRight />
+          </div>
+        </article>
+      </section>
     </>
   )
 }

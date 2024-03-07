3e8372a073e6cc2851b1b7701536ca37f5e6e5eb
Victor Villazón <victorzn23@gmail.com>
2024-03-06 23:08:13 -0500

    semana5, dia02, invoice app part2

--- semana04/dia03-nextjs-budgets-app/app/invoices/[id]/page.jsx
+++ semana04/dia03-nextjs-budgets-app/app/invoices/[id]/page.jsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Link from "next/link"
 
 import BaseTag from "@/components/shared/BaseTag";
@@ -50,7 +52,82 @@ export default function InvoiceDetail({ params }) {
         </div>
       </header>
 
-      {id}
+      <section
+        className="bg-slate-700 rounded-lg text-white p-10 mt-5"
+      >
+        <div className="flex justify-between w-full">
+          <div className="text-3xl">
+            <span className="text-slate-400">#</span>
+            <span className="font-extrabold">XM1156</span>
+            <div className="text-xl mt-1 text-slate-400">Graphic Design</div>
+          </div>
+          <div className="text-sm text-right">
+            <div>streetAddress</div>
+            <div>city</div>
+            <div>postCode</div>
+            <div>country</div>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-3 justify-center w-full">
+          <div className="flex flex-col gap-10">
+            <div>
+              <div className="text-base text-slate-400">Invoice Date</div>
+              <div className="text-xl font-semibold">date</div>
+            </div>
+            <div>
+              <div className="text-base text-slate-400">Invoice Due</div>
+              <div className="text-xl font-semibold">date</div>
+            </div>
+          </div>
+
+          <div>
+            <div className="text-base text-slate-400">Bill to</div>
+            <div className="text-sm">clientName</div>
+            <div className="text-sm">streetaddress</div>
+            <div className="text-sm">city</div>
+            <div className="text-sm">postCode</div>
+            <div className="text-sm">country</div>
+          </div>
+
+          <div>
+            <div className="text-base text-slate-400">Send to</div>
+            <div className="text-xl font-semibold">
+              email@email.com
+            </div>
+          </div>
+        </div>
+
+        {/* TODO: Terminar de maquetar esta tabla */}
+
+        <table>
+          <thead>
+            <tr>
+              <td>Item Name</td>
+              <td>QTY</td>
+              <td>Price</td>
+              <td>Total</td>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>item.name</td>
+              <td>qty</td>
+              <td>price</td>
+              <td>$ total</td>
+            </tr>
+          </tbody>
+          <tfoot>
+            <tr>
+              <td colSpan={2}>Amount Due</td>
+              <td colSpan={2}>
+                $ 0.00
+              </td>
+            </tr>
+          </tfoot>
+        </table>
+      </section>
+
     </section>
   )
 }

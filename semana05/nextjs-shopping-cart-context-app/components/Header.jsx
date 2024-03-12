c959ff4682a7f1b0159a14072a68b1e31ce1697c
Victor Villazón <victorzn23@gmail.com>
2024-03-11 22:07:36 -0500

    semana5, dia01, shopping cart context app

--- semana05/nextjs-shopping-cart-context-app/components/Header.jsx
+++ semana05/nextjs-shopping-cart-context-app/components/Header.jsx
@@ -1,10 +1,12 @@
 'use client'
 
 import { CartContext } from "@/context/CartContext"
-import { useContext } from "react"
+import { useContext, useState } from "react"
 
 const Header = () => {
   const { cart } = useContext(CartContext)
+
+  const [open, setOpen] = useState(false)
 
   return (
     <header className="bg-lime-600 text-white py-4 px-6 fixed w-full">
@@ -13,9 +15,19 @@ const Header = () => {
           Shopping Cart
         </h1>
 
-        <button>Cart</button>
+        <button onClick={() => setOpen(!open)}>Cart</button>
 
-        <div>{JSON.stringify(cart)}</div>
+        <div
+          className={`fixed top-0 right-0 z-40 h-screen p-4 overflow-y-auto transition-transform bg-white w-80 text-black ${open ? 'transform-none' : 'translate-x-full'}`}
+        >
+          <div className="flex justify-between">
+            <h5 className="flex text-base font-semibold text-gray-500">Cart</h5>
+
+            <button onClick={() => setOpen(false)}>❌</button>
+          </div>
+
+          <pre>{JSON.stringify(cart.map(p => ({ title: p.title })), null, 2)}</pre>
+        </div>
       </div>
     </header>
   )

37797ade688e14ac1dd5592674b53dd1a175372c
Victor Villazón <victorzn23@gmail.com>
2024-03-13 23:06:37 -0500

    semana5, dia03, nextauth simple credentials

---
+++ semana05/dia03-nextauth-simple-credentials/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,24 @@
+import NextAuth from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials"
+
+export const authOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email", placeholder: "test@example.com"},
+        password: { label: "Password", type: "passwoord"},
+      },
+      async authorize (credentials, req) {
+        // Aquí es donde haremos el llamdao al endpoint de autenticación
+        return {
+          email: 'victor@gmail.com'
+        }
+      }
+    }),
+  ]
+}
+
+const handler = NextAuth(authOptions)
+
+export { handler as GET, handler as POST }

2122518e7145abc8f0b1cb8276f195d7425481e1
Victor Villazón <victorzn23@gmail.com>
2024-03-15 07:51:39 -0500

    demo de login

--- semana05/dia03-nextauth-simple-credentials/app/page.js
+++ semana05/dia03-nextauth-simple-credentials/app/page.js
@@ -2,12 +2,14 @@
 
 import { useState } from "react";
 
-import { signIn } from "next-auth/react"
+import { signIn, useSession, signOut } from "next-auth/react"
 
 export default function Home() {
+  const { data: session } = useSession();
+
   const [form, setForm] = useState({
-    email: '',
-    password: ''
+    email: 'ejemplo@gmail.com',
+    password: 'mipass123'
   })
 
   const handleLogin = async (event) => {
@@ -24,19 +26,37 @@ export default function Home() {
     console.log(responseNextAuth)
   }
 
+  const getStudents = async() =>{
+    const response = await fetch('http://localhost:3000/api/students')
+
+    const data = await response.json()
+
+    console.log(data)
+  }
+
   return (
-    <form onSubmit={handleLogin}>
-      <input
-        type="email"
-        onChange={(event) => setForm(prev => ({ ...prev, email: event.target.value }))}
-        value={form.email}
-      />
-      <input
-        type="password"
-        onChange={(event) => setForm(prev => ({ ...prev, password: event.target.value }))}
-        value={form.password}
-      />
-      <button>Login</button>
-    </form>
+    <>
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          onChange={(event) => setForm(prev => ({ ...prev, email: event.target.value }))}
+          value={form.email}
+        />
+        <input
+          type="password"
+          onChange={(event) => setForm(prev => ({ ...prev, password: event.target.value }))}
+          value={form.password}
+          />
+        <button>Login</button>
+      </form>
+
+      {session && (
+        <div>
+          {JSON.stringify(session)}
+          <button onClick={getStudents}>GetStudents</button>
+          <button onClick={signOut}>Logout</button>
+        </div>
+      )}
+    </>
   );
 }
